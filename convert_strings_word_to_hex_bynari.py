3c1229bbb206052270a1d2f86f38fd52cacfe3e5
lundpc <lundpc@test.com>
2022-10-05 02:49:45 -0500

    version3 strings to hex binary

--- convert_strings_word_to_hex_bynari.py
+++ convert_strings_word_to_hex_bynari.py
@@ -1,9 +1,13 @@
 import binascii
 
-word = "comoooo estassss bienbenidos a mugredev"
+word = "comoooo estassss"
 word_2byte = bytes(word, encoding='ascii')
 word_hex = binascii.hexlify(word_2byte)
-print(word_hex)
+
+correct = ''.join('\\x{:02x}'.format(c) for c in word_2byte)
+
+print("normal",word_hex)
+print("correct",correct)
 
 # output                :  b'636f6d6f6f6f****
 # the output correct is :  b'\x63\x6f\x6d\x6f\x6f\x6f\x****
