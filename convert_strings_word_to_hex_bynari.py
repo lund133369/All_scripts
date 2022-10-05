cc062eb287483654bfcf88fcf227d0cbc523d211
lundpc <lundpc@test.com>
2022-10-05 02:41:02 -0500

    version 2 string to hex binary

--- convert_strings_word_to_hex_bynari.py
+++ convert_strings_word_to_hex_bynari.py
@@ -4,3 +4,6 @@ word = "comoooo estassss bienbenidos a mugredev"
 word_2byte = bytes(word, encoding='ascii')
 word_hex = binascii.hexlify(word_2byte)
 print(word_hex)
+
+# output                :  b'636f6d6f6f6f****
+# the output correct is :  b'\x63\x6f\x6d\x6f\x6f\x6f\x****
